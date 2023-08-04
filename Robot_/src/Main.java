eb28bdc42f25fa80bf66bce31e92a23e6f97ebd1
Huy Nguyen <huyite.vn@gmail.com>
2023-08-04 16:07:50 +0700

    ok

--- Robot_/src/Main.java
+++ Robot_/src/Main.java
@@ -5,7 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		 
+		 String[] args2 = new String[args.length + 1];
+		System.arraycopy(args, 0, args2, 0, args.length);
+		args2[args.length] = args;
 		
 	} 
 
